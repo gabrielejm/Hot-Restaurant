f7d74ac4112f3b2c54b893bd286942bbddff5720
Justin Gabriele <gabrielejustin@yahoo.com>
2020-11-11 13:21:04 -0500

    starting code for express and bodyparser

---
+++ server.js
@@ -0,0 +1,22 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.use(function (req, res) {
+  res.setHeader("Content-Type", "text/plain");
+  res.write("you posted:\n");
+  res.end(JSON.stringify(req.body, null, 2));
+});
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
